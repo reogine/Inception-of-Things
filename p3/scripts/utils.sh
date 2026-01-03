d6e0ace778a58f79446dea7a15a41732c38c3f09
Brahim Berkasse <berkassebrahim@gmail.com>
2026-01-03 19:20:34 +0100

    update port

--- p3/scripts/utils.sh
+++ p3/scripts/utils.sh
@@ -7,7 +7,7 @@
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
 # forward argoCD port 
-kubectl port-forward -n argocd svc/argocd-server 8080:443
+kubectl port-forward -n argocd svc/argocd-server 8080:80
 
 # forward playground port 
 kubectl port-forward -n dev svc/playground 8888:8888
